ac33fffedad802bc74ea65c4b38e0e33c2284e59
KevMichelBiab <codingpurposesnew@gmail.com>
2025-04-01 22:43:14 +0200

    Recommencons depuis le debut

--- src/Model.java
+++
@@ -1,8 +0,0 @@
-public class Model {
-    private static final int HAUTEUR = 40;
-    private static final int LARGEUR = 60;
-    private zone[][] cases;
-
-
-}
-
--- src/Vue.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class Vue {
-    private JFrame frame;
-
-
-}
