48939e6b96f6377811974a3a748addb6e7ab8f90
kevbiab <codingpurposesnew@gmail.com>
2025-04-02 12:25:22 +0200

    tried to change the color of each zone depending on the state

---
+++ src/Controller/control.java
@@ -0,0 +1,21 @@
+package Controller;
+import model.*;
+import view.*;
+import Controller.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class control implements ActionListener {
+    public island mod;
+
+    public control(island mod){
+        this.mod = mod;
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        mod.init();
+    }
+
+}
--- src/view/Buttons.java
+++ src/view/Buttons.java
@@ -1,4 +1,29 @@
 package view;
+import model.*;
+import Controller.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Buttons extends JPanel implements Observer{
 
-public class Buttons {
+    private island mod;
+
+    public Buttons(island mod){
+        this.mod = mod;
+
+        JButton finDeTour = new JButton(">");
+        this.add(finDeTour);
+
+        control ctrl = new control(this.mod);
+        finDeTour.addActionListener(ctrl);
+
+
+    }
+
+
+    @Override
+    public void update() {
+        repaint();
+    }
 }
